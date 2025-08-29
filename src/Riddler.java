185a9247a5d006b5989421e7748cf43a29d73c91
lkassaei <lily.kassaei@menloschool.org>
2025-08-28 19:29:57 -0700

    Finished the second part of the Riddler

--- src/Riddler.java
+++ src/Riddler.java
@@ -7,35 +7,60 @@
  * Completed by: Lily Kassaei
  */
 public class Riddler {
+    public static final int CAESAR_SHIFT = 17;
+    public static final int UNICODE_SHIFT = 9919;
+    public static final int ALPHABET_SIZE = 26;
+    public static final int BINARY_NUMBER_SIZE = 8;
 
+    // Caesar Cipher
     public String decryptOne(@org.jetbrains.annotations.NotNull String encrypted) {
+        // Return nothing if given an empty encryption
+        if (encrypted.isEmpty()) {
+            return "";
+        }
+
+        // Create space for final answer
         String decrypted = "";
 
-        // TODO: Complete the decryptOne() function.
-        // Go through the encryption
-        for (int i = 0; i < encrypted.length(); i++) {
-            // Extract the encrypted character
-            char encryptedChar = encrypted.charAt(i);
-            // Find if the character is upper lower or punctuation to ensure correct shift
-            if (encryptedChar >= 'A' && encryptedChar <= 'Z') {
-                decrypted += (char)('A' + (encryptedChar - 'A' - 17 + 26) % 26);
-            }
-            else if (encryptedChar >= 'a' && encryptedChar <= 'z') {
-                decrypted += (char)('a' + (encryptedChar - 'a' - 17 + 26) % 26);
-            }
-            else {
-                decrypted += encryptedChar;
-            }
+        // Go through the encryption and add each correctly shifted char to our final answer
+        for (char encryptedChar : encrypted.toCharArray()) {
+            decrypted += caesarShiftChar(encryptedChar);
         }
+
+        // Return final answer and print for debugging
         System.out.println(decrypted);
         return decrypted;
     }
 
+    // Function that shifts a char by correct shift and returns it
+    public char caesarShiftChar(char encryptedChar) {
+        // If char is Uppercase
+        if (encryptedChar >= 'A' && encryptedChar <= 'Z') {
+            // Shift char within Uppercase ASCII values
+            return (char)('A' + (encryptedChar - 'A' - CAESAR_SHIFT + ALPHABET_SIZE) % ALPHABET_SIZE);
+        }
+        // If char is lowercase
+        else if (encryptedChar >= 'a' && encryptedChar <= 'z') {
+            // Shift char within Lowercase ASCII values
+            return (char)('a' + (encryptedChar - 'a' - CAESAR_SHIFT + ALPHABET_SIZE) % ALPHABET_SIZE);
+        }
+        // Punctuation remains unchanged
+        else {
+            return encryptedChar;
+        }
+    }
+
+    // ASCII number to char cipher
     public String decryptTwo(String encrypted) {
+        // Return nothing if given an empty encryption
+        if (encrypted.isEmpty()) {
+            return "";
+        }
+
+        // Create space for final answer and temp string
         String decrypted = "";
         String str = "";
 
-        // TODO: Complete the decryptTwo() function.
         // Take ASCII equivalent of every num in between the spaces
         for (int i = 0; i < encrypted.length(); i++) {
             // Find first space
@@ -59,20 +84,27 @@ public class Riddler {
             }
         }
 
+        // Return final answer and print for debugging
         System.out.println(decrypted);
         return decrypted;
     }
 
+    // Binary number to char cipher
     public String decryptThree(String encrypted) {
+        // Return nothing if given an empty encryption
+        if (encrypted.isEmpty()) {
+            return "";
+        }
+
+        // Create space for final answer
         String decrypted = "";
 
-        // TODO: Complete the decryptThree() function.
         // Take groups of 8, convert binary to ASCII
-        for (int i = 0; i < encrypted.length(); i+=8) {
+        for (int i = 0; i < encrypted.length(); i+=BINARY_NUMBER_SIZE) {
             // Make sure we are in bounds
-            if (i <= encrypted.length() - 9) {
+            if (i <= encrypted.length() - BINARY_NUMBER_SIZE - 1) {
                 // Take group of 8
-                String str = encrypted.substring(i, i + 8);
+                String str = encrypted.substring(i, i + BINARY_NUMBER_SIZE);
                 // Get base 10 number from the String in base 2
                 int num = Integer.parseInt(str, 2);
                 // Convert num into ASCII
@@ -80,20 +112,31 @@ public class Riddler {
                 decrypted += letter;
             }
         }
+
+        // Return final answer and print for debugging
         System.out.println(decrypted);
         return decrypted;
     }
 
+    // Unicode dingbat to char cipher
     public String decryptFour(String encrypted) {
+        // Return nothing if given an empty encryption
+        if (encrypted.isEmpty()) {
+            return "";
+        }
+
+        // Create space for final answer
         String decrypted = "";
 
-        // TODO: Complete the decryptFour() function.
-
         for (char c : encrypted.toCharArray()) {
-            // This is the Unicode for it but I don't know how to get from Unicode to text or ascii
-            String str = Integer.toHexString(c);
-            decrypted += str + " ";
+            // Find Unicode number and shift it down into alphabetical values
+            int num = c;
+            char letter = (char)(num - UNICODE_SHIFT);
+            // Add to final answer
+            decrypted += letter;
         }
+
+        // Return final answer and print for debugging
         System.out.println(decrypted);
         return decrypted;
     }
